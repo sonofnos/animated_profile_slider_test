125c33154f35801b8e274d9930f8984aa22ac16b
Christian Emeka <54124037+sonofnos@users.noreply.github.com>
2025-03-06 13:27:33 +0100

    prototype

--- lib/main.dart
+++ lib/main.dart
@@ -1,154 +1,45 @@
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // TRY THIS: Try running your application with "flutter run". You'll see
-//         // the application has a purple toolbar. Then, without quitting the app,
-//         // try changing the seedColor in the colorScheme below to Colors.green
-//         // and then invoke "hot reload" (save your changes or press the "hot
-//         // reload" button in a Flutter-supported IDE, or press "r" if you used
-//         // the command line to start the app).
-//         //
-//         // Notice that the counter didn't reset back to zero; the application
-//         // state is not lost during the reload. To reset the state, use hot
-//         // restart instead.
-//         //
-//         // This works for code too, not just values: Most code changes can be
-//         // tested with just a hot reload.
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // TRY THIS: Try changing the color here to a specific color (to
-//         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-//         // change color while the other colors stay the same.
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           //
-//           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-//           // action in the IDE, or press "p" in the console), to see the
-//           // wireframe for each widget.
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text('You have pushed the button this many times:'),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+/// Main entry point for the application.
+/// Sets up landscape orientation before launching the app.
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  // Force landscape orientation for better carousel display
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]).then((_) {
+    runApp(const MyApp());
+  });
 }
 
+/// Root application widget that sets up the MaterialApp with theme settings.
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Animated Profile Slider',
       theme: ThemeData(primarySwatch: Colors.blue, brightness: Brightness.dark),
-      home: SimpleAnimatedList(),
+      home: const SimpleAnimatedList(),
     );
   }
 }
 
+/// Wrapper widget that creates a Scaffold with the main SliceAnimatedList.
 class SimpleAnimatedList extends StatelessWidget {
   const SimpleAnimatedList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SliceAnimatedList());
+    return const Scaffold(body: SliceAnimatedList());
   }
 }
 
+/// Main animated carousel component that displays items in a horizontal row
+/// with the selected item centered and scaled up.
 class SliceAnimatedList extends StatefulWidget {
   const SliceAnimatedList({super.key});
 
@@ -156,26 +47,158 @@ class SliceAnimatedList extends StatefulWidget {
   _SliceAnimatedListState createState() => _SliceAnimatedListState();
 }
 
-class _SliceAnimatedListState extends State<SliceAnimatedList> {
-  final GlobalKey<AnimatedListState> listKey = GlobalKey<AnimatedListState>();
-  List<int> _items = [];
-  int counter = 0;
+class _SliceAnimatedListState extends State<SliceAnimatedList>
+    with TickerProviderStateMixin {
+  // Constants for animations and layout
+  static const double _ITEM_WIDTH = 150.0;
+  static const double _ITEM_SPACING = 180.0;
+  static const double _MAX_SCALE = 1.0;
+  static const double _MIN_SCALE = 0.3;
+  static const double _SCALE_FACTOR = 0.2;
+  static const Duration _ANIMATION_DURATION = Duration(milliseconds: 300);
+  static const Duration _BUTTON_PULSE_DURATION = Duration(milliseconds: 1500);
 
-  Widget slideIt(BuildContext context, int index, animation) {
-    int item = _items[index];
+  // Item data and state
+  final List<int> _items = [];
+  int _counter = 0;
+  int _selectedIndex = 2; // Default selected item (middle of initial 5 items)
+
+  // Button animation controller
+  late AnimationController _buttonAnimationController;
+  late Animation<double> _buttonAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the carousel with 5 items
+    _populateInitialItems();
+
+    // Setup button animation controller for pulsing effect
+    _initializeButtonAnimation();
+  }
+
+  /// Populates initial items in the carousel
+  void _populateInitialItems() {
+    for (int i = 0; i < 5; i++) {
+      _items.add(_counter++);
+    }
+  }
+
+  /// Initializes the animation for the navigation buttons
+  void _initializeButtonAnimation() {
+    _buttonAnimationController = AnimationController(
+      vsync: this,
+      duration: _BUTTON_PULSE_DURATION,
+    )..repeat(reverse: true);
+
+    _buttonAnimation = Tween<double>(begin: 1.0, end: 1.3).animate(
+      CurvedAnimation(
+        parent: _buttonAnimationController,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _buttonAnimationController.dispose();
+    super.dispose();
+  }
+
+  /// Updates the selected item index and triggers animation
+  void _selectItem(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  /// Navigates to the previous item if not at the beginning
+  void _selectPreviousItem() {
+    if (_selectedIndex > 0) {
+      _selectItem(_selectedIndex - 1);
+    }
+  }
+
+  /// Navigates to the next item if not at the end
+  void _selectNextItem() {
+    if (_selectedIndex < _items.length - 1) {
+      _selectItem(_selectedIndex + 1);
+    }
+  }
+
+  /// Builds an individual card item for the carousel
+  Widget buildCardItem(
+    BuildContext context,
+    int item,
+    TextStyle? textStyle,
+    double scale,
+  ) {
+    return SizedBox(
+      width: _ITEM_WIDTH,
+      child: Card(
+        color: Colors.primaries[item % Colors.primaries.length],
+        child: Center(child: Text('Item $item', style: textStyle)),
+      ),
+    );
+  }
+
+  /// Calculates the scale for an item based on its distance from the selected item
+  double _calculateItemScale(int index) {
+    final int distanceFromCenter = (index - _selectedIndex).abs();
+    return (_MAX_SCALE - (distanceFromCenter * _SCALE_FACTOR)).clamp(
+      _MIN_SCALE,
+      _MAX_SCALE,
+    );
+  }
+
+  /// Builds a carousel item with position and scale animations
+  Widget _buildCarouselItem(BuildContext context, int index) {
+    final double scale = _calculateItemScale(index);
+    final double offsetX = (index - _selectedIndex) * _ITEM_SPACING;
+
     TextStyle? textStyle = Theme.of(context).textTheme.headlineMedium;
-    return SlideTransition(
-      position: Tween<Offset>(
-        begin: const Offset(0, -1),
-        end: Offset(0, 0),
-      ).animate(animation),
-      child: SizedBox(
-        height: 128.0,
-        child: Card(
-          color: Colors.primaries[item % Colors.primaries.length],
-          child: Center(child: Text('Item $item', style: textStyle)),
+    int item = _items[index];
+
+    return AnimatedPositioned(
+      duration: _ANIMATION_DURATION,
+      curve: Curves.easeInOut,
+      left: MediaQuery.of(context).size.width / 2 - _ITEM_WIDTH / 2 + offsetX,
+      child: GestureDetector(
+        onTap: () => _selectItem(index),
+        child: AnimatedScale(
+          scale: scale,
+          duration: _ANIMATION_DURATION,
+          curve: Curves.easeInOut,
+          child: buildCardItem(context, item, textStyle, scale),
         ),
       ),
+    );
+  }
+
+  /// Builds a navigation button with pulse animation
+  Widget _buildNavigationButton({
+    required IconData icon,
+    required VoidCallback onPressed,
+    required bool isLeft,
+  }) {
+    return AnimatedBuilder(
+      animation: _buttonAnimationController,
+      builder: (context, child) {
+        return Transform.scale(
+          scale: _buttonAnimation.value,
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Colors.black54,
+              shape: BoxShape.circle,
+            ),
+            child: IconButton(
+              icon: Icon(icon, color: Colors.white),
+              onPressed: onPressed,
+              iconSize: 30,
+            ),
+          ),
+        );
+      },
     );
   }
 
@@ -184,54 +207,38 @@ class _SliceAnimatedListState extends State<SliceAnimatedList> {
     return Column(
       children: <Widget>[
         SizedBox(
-          height: MediaQuery.of(context).size.height / 2,
-          child: AnimatedList(
-            key: listKey,
-            initialItemCount: _items.length,
-            itemBuilder: (context, index, animation) {
-              return slideIt(context, index, animation);
-            },
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(color: Colors.greenAccent),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    listKey.currentState!.insertItem(
-                      0,
-                      duration: const Duration(milliseconds: 500),
-                    );
-                    _items = [counter++, ..._items];
-                  });
-                },
-                child: Text(
-                  "Add item to first",
-                  style: TextStyle(color: Colors.black, fontSize: 20),
-                ),
+          height: 150.0,
+          width: MediaQuery.of(context).size.width,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              // Generate all carousel items
+              ...List.generate(
+                _items.length,
+                (index) => _buildCarouselItem(context, index),
               ),
-              TextButton(
-                onPressed: () {
-                  if (_items.length <= 1) return;
-                  listKey.currentState!.removeItem(
-                    0,
-                    (_, animation) => slideIt(context, 0, animation),
-                    duration: const Duration(milliseconds: 500),
-                  );
-                  setState(() {
-                    _items.removeAt(0);
-                  });
-                },
-                child: Text(
-                  "Remove first item",
-                  style: TextStyle(color: Colors.black, fontSize: 20),
+
+              // Left navigation button - only show if not at first item
+              if (_selectedIndex > 0)
+                Positioned(
+                  left: 20,
+                  child: _buildNavigationButton(
+                    icon: Icons.arrow_back_ios,
+                    onPressed: _selectPreviousItem,
+                    isLeft: true,
+                  ),
                 ),
-              ),
+
+              // Right navigation button - only show if not at last item
+              if (_selectedIndex < _items.length - 1)
+                Positioned(
+                  right: 20,
+                  child: _buildNavigationButton(
+                    icon: Icons.arrow_forward_ios,
+                    onPressed: _selectNextItem,
+                    isLeft: false,
+                  ),
+                ),
             ],
           ),
         ),
